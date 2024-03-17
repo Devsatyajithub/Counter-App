ba7a66e222d15fd25b7a9839669bd6f0e750b4ca
satyajit <satyajitnayak.dibun@gmail.com>
2024-03-17 16:34:34 +0530

    base url add

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Counter-App/",
   build: {
     outDir:"./docs",
   }
